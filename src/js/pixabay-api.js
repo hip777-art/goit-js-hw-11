8fedab7611aa09d25a93a1116464e7c4753f45a1
hip777-art <arkhip24032000@gmail.com>
2026-03-21 20:15:08 +0200

    Create pixabay-api.js

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,55 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+
+const galleryContainer = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+        </a>
+        <div class="info">
+          <p><b>Likes</b>${likes}</p>
+          <p><b>Views</b>${views}</p>
+          <p><b>Comments</b>${comments}</p>
+          <p><b>Downloads</b>${downloads}</p>
+        </div>
+      </li>
+    `
+    )
+    .join('');
+
+  galleryContainer.innerHTML = markup;
+
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryContainer.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.add('is-visible');
+}
+
+export function hideLoader() {
+  loader.classList.remove('is-visible');
+}
